1c0bf032071e093a5a01f7a9be4fec3ae9c51476
Rahul Grover <rgrover4@illinois.edu>
2022-02-12 16:38:08 -0600

    Lab 3 Complete

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -14,17 +14,30 @@
 int main()
 {
     // Declare variables
+    int num;
+    float firstW;
+    float secondW;
 
     // Prompt user for input
-
+    printf("Enter the values of n, omega1, and omega2 in that order \n");
+    
     // Get user input
-
+    scanf("%d%f%f", &num, &firstW, &secondW);
+    
     // Compute function output
     /* for i from 0 to n-1
      *     compute and print xi and f(xi)
      *     use sin() function from math.h
      */
+    if(num < 1){
+        printf("Invalid input");
+        return 0;
+    }
 
+    for(int i = 0; i < num; i++){
+        float xi = (i * M_PI)/num;
+        float out = sin(firstW * xi) + 0.5 * sin(secondW * xi);
+        printf("(%f, %f)\n", xi, out);
+    }
     return 0;
 }
-
