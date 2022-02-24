eb6b00a9cce2127d831bf26ed2904b83c809ee5b
Rahul Grover <rgrover4@illinois.edu>
2022-02-24 17:20:56 -0600

    MP4 Complete

--- mp4/semiprime.c
+++ mp4/semiprime.c
@@ -11,12 +11,13 @@ int is_prime(int number)
 {
     int i;
     if (number == 1) {return 0;}
+    if (number == 2) {return 1;}
     for (i = 2; i < number; i++) { //for each number smaller than it
         if (number % i == 0) { //check if the remainder is 0
-            return 1;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 
@@ -34,9 +35,10 @@ int print_semiprimes(int a, int b)
         for (j = 2; j <= i; j++) {
             if (i%j == 0) {
                 if (is_prime(j)) {
-                    k = i%j;
+                    k = i/j;
                     if (is_prime(k)) {
                         printf("%d ", i);
+                        break;
                     }
                 }
             }
