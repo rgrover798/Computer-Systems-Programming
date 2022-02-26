fc24eb357a9521fc15bf888d4e431963fab1a6e1
Rahul Grover <rgrover4@illinois.edu>
2022-02-26 17:40:22 -0600

    Lab 5 updated

--- lab5/dice.c
+++ lab5/dice.c
@@ -1,5 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h> 
 //Function for generating three d6 rolls
 
-void roll_three(int* one, int* two, int* three){
-
+void roll_three(int *one, int *two, int *three){
+    *one = rand() % 6 + 1;
+    *two = rand() % 6 + 1;
+    *three = rand() % 6 + 1;
 }
--- lab5/main.c
+++ lab5/main.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <dice.h>
+
 /* Code to simulate rolling three six-sided dice (D6)
  * User first types in seed value
  * Use seed value as argument to srand()
@@ -8,4 +12,33 @@
  * Otherwise print "\n"
  */
 
+int main() {
+    
+    //seed var
+    int seedVal;
+
+    //roll_three vars
+    int firstVal;
+    int secondVal;
+    int thirdVal;
+
+    //I/O
+    printf("Enter Seed: ");
+    scanf("%d", &seedVal);
+    
+    //set up RNG and generate values
+    srand(seedVal);
+    roll_three(&firstVal, &secondVal, &thirdVal);
+
+    //print output
+    if(firstVal == secondVal && secondVal == thirdVal){
+        printf("%d %d %d Triple!", &firstVal, &secondVal, &thirdVal);
+    } else if (firstVal == secondVal || firstVal == thirdVal || secondVal == thirdVal){
+        printf("%d %d %d Double!", &firstVal, &secondVal, &thirdVal);
+    } else {
+        printf("%d %d %d", &firstVal, &secondVal, &thirdVal);
+    }
+
+
+}
 
