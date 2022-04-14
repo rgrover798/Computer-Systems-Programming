7a1d3798f61789832445c533a41a158223319a37
Rahul Grover <rgrover4@illinois.edu>
2022-04-13 22:41:50 -0500

    Testing

--- mp10/sparsemat.c
+++ mp10/sparsemat.c
@@ -59,19 +59,20 @@ sp_tuples * load_tuples(char* input_file)
      * -----------
      */
 
+    //variables (some are temporary) for swapping in insertion sort as well as looping
     int tempRow, tempCol, testNodeIndex, swapNodeIndex, i, j;
     double tempVal;
     sp_tuples_node * currentNode = myTuple -> tuples_head;
     sp_tuples_node * testNode, * swapNode;
 
-
+    //insertion sort through linked list to sort values
      for(i = 0; i < myTuple -> nz; i++){
          if(i != 0){
              currentNode = currentNode -> next;
          }
          testNode = currentNode;
          swapNode = currentNode;
-         for(sj = 0; j < myTuple -> nz; j++){
+         for(j = 0; j < myTuple -> nz; j++){
              testNodeIndex = testNode -> row * myTuple -> m + testNode -> col;
              swapNodeIndex = swapNode -> row * myTuple -> m + swapNode -> col;
              if(testNodeIndex < swapNodeIndex){
@@ -118,105 +119,93 @@ double gv_tuples(sp_tuples * mat_t,int row,int col)
 }
 
 
-
+//read over but not DONE
 void set_tuples(sp_tuples * mat_t, int row, int col, double value)
 {
-    //segment to remove node from linked list if value is 0
-    //set currentNode to head
     sp_tuples_node * currentNode = mat_t -> tuples_head;
-    //set prevNode and nextNode to NULL for now
     sp_tuples_node * prevNode = NULL, * nextNode = NULL;
-    //if value is 0, loop through linked list and look for corresponding row and column
-    if(value == 0){
+
+    //case if input value is 0
+    if(value == 0 && gv_tuples(mat_t, row, col) != 0){
         while(currentNode != NULL){
             nextNode = currentNode -> next;
             if(currentNode -> row == row && currentNode -> col == col){
-                //if prevNode is still NULL, we are at the beginning of the linked list
                 if(prevNode == NULL){
-                    //make the next node from the beginning, the new head
                     mat_t -> tuples_head = nextNode;
-                    //free associated memory, set to NULL, and break
                     free(currentNode);
                     currentNode = NULL;
+                    mat_t -> nz -= 1;
                     break;
-                //if prevNode is not NULL, we are anywhere but the beginning of the linked list
                 } else {
-                    //set prevNode to point to the nextNode from the currnet node we are looking at
                     prevNode -> next = nextNode;
-                    //free associated memory, set to NULL, and break
                     free(currentNode);
                     currentNode = NULL;
+                    mat_t -> nz -= 1;
                     break;
                 }
-            // if the row and col doesnt match, set prevNode to the currentNode we just looked at and set currentNode to the next node
             } else {
                 prevNode = currentNode;
                 currentNode = currentNode -> next;
             }
         }
-        //if the whole process works, then decrement nz since we now have one less value in our linked list
-        mat_t -> nz -= 1;
-    } 
+    }
 
-    //segement to check if index is in linked list already AND value is non-zero
-    //set currentNode back to the head
-    currentNode = mat_t -> tuples_head;
-    //loop through linked list till NULL is reached, if row and col match, set old value to new value and break
-    while(currentNode != NULL){
-        if(currentNode -> row == row && currentNode -> col == col){
-            currentNode -> value = value;
-            break;
-        //else, increment to the next value in the linked list
-        } else {
-            currentNode = currentNode -> next;
-        }
-    } 
-    //segment to ADDRESS if index is NOT in linked list already AND value is non-zero
-    //set currentNode to the head of the matrix and prevNode back to NULL
-    currentNode = mat_t -> tuples_head;
-    prevNode = NULL;
-    //loop through the linked list till NULL is reached
-    while(currentNode != NULL){
-        //get index of current node and index of the node you intend to place 
-        int curNodeIndex = currentNode -> row * mat_t -> m + currentNode -> col;
-        int swapNodeIndex = row * mat_t -> m + col;
-        //if the prevNode is null, meaning we are at the head of the list, and the current index is greater
-        //than what we want to insert, make what we want to insert the new head
-        if(prevNode == NULL && curNodeIndex > swapNodeIndex){
-            //allocate memory for new node and attach values passed through function
-            sp_tuples_node * newHead = malloc(sizeof(sp_tuples_node));
-            newHead -> row = row;
-            newHead -> col = col;
-            newHead -> value = value;
-            //set mat_t head to this new head node
-            mat_t -> tuples_head = newHead;
-            //set the next value to the current node because that is no longer our head and this new head points to it
-            newHead -> next = currentNode; 
-            //if all this goes through, increment the number of values in this linked list by 1 since we added a node and break
-            mat_t -> nz += 1;
-            break;
-        //if prevNode is not NULL, we are anywhere else in the linked list so insert the value in the proper spot
-        } else {
-            //if the current node index > the index of the node we want to insert, we put it before it
-            if(curNodeIndex > swapNodeIndex){
-                //allocate memory for new node and attach values passed through function
-                sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
-                newNode -> row = row;
-                newNode -> col = col;
-                newNode -> value = value;
-                //make this new node point to our current node since it is being placed before it
-                newNode -> next = currentNode;
-                //update pointer of the old node that was behind current node to new node since it took its spot
-                prevNode -> next = newNode;
-                //if all this goes through, increment the number of values in this linked list by 1 since we added a node and break
+    //case if input value is non-zero and node exists 
+    if(value != 0 && gv_tuples(mat_t, row, col) != 0){
+        currentNode = mat_t -> tuples_head;
+        while(currentNode != NULL){
+            if(currentNode -> row == row && currentNode -> col == col){
+                currentNode -> value = value;
+                break;
+            } else {
+                currentNode = currentNode -> next;
+            }
+        } 
+    }
+    
+    //case if input value is non-zero and node does NOT exist
+    if(value != 0 && gv_tuples(mat_t, row, col) == 0){
+        //case if matrix is non empty
+        currentNode = mat_t -> tuples_head;
+        prevNode = NULL;
+        while(currentNode != NULL){
+            int curNodeIndex = currentNode -> row * mat_t -> m + currentNode -> col;
+            int swapNodeIndex = row * mat_t -> m + col;
+            if(prevNode == NULL && curNodeIndex > swapNodeIndex){
+                sp_tuples_node * newHead = malloc(sizeof(sp_tuples_node));
+                newHead -> row = row;
+                newHead -> col = col;
+                newHead -> value = value;
+                mat_t -> tuples_head = newHead;
+                newHead -> next = currentNode; 
                 mat_t -> nz += 1;
                 break;
+            } else {
+                if(curNodeIndex > swapNodeIndex){
+                    sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
+                    newNode -> row = row;
+                    newNode -> col = col;
+                    newNode -> value = value;
+                    newNode -> next = currentNode;
+                    prevNode -> next = newNode;
+                    mat_t -> nz += 1;
+                    break;
+                }
             }
+            prevNode = currentNode;
+            currentNode = currentNode -> next;
         }
-        //if nothing works, then set prevNode to the currentNode we just checked and set currentNode to the next node in the linked list
-        prevNode = currentNode;
-        currentNode = currentNode -> next;
+        //case if matrix is empty
+        if(mat_t -> tuples_head == NULL){
+            sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
+                    newNode -> row = row;
+                    newNode -> col = col;
+                    newNode -> value = value;
+                    newNode -> next = NULL;
+                    mat_t -> nz += 1;
+        }
     }
+    
 }
 
 
@@ -255,7 +244,7 @@ void save_tuples(char * file_name, sp_tuples * mat_t)
 
 sp_tuples * add_tuples(sp_tuples * matA, sp_tuples * matB)
 {
-    sp_tuples * matC;
+    sp_tuples * matC = NULL;
     if(matA -> m == matB -> m && matA -> n == matB -> n && matA != NULL && matB != NULL){
         matC = malloc(sizeof(sp_tuples));
         matC -> m = matA -> m;
@@ -265,53 +254,35 @@ sp_tuples * add_tuples(sp_tuples * matA, sp_tuples * matB)
 
         sp_tuples_node * currentNodeA = matA -> tuples_head;
         sp_tuples_node * currentNodeB = matB -> tuples_head;
-        sp_tuples_node * prevNode;
+        sp_tuples_node * currentNode = NULL;
 
-        int curRowA, curColA, curRowB, curColB, i, j, sum;
-        for(i = 0; i < matA -> nz; i++){
-            currentNodeB = matB -> tuples_head;
-            for(j = 0; j < matB -> nz; j++){
-                curRowA = currentNodeA -> row;
-                curColA = currentNodeA -> col;
-                curRowB = currentNodeB -> row;
-                curColB = currentNodeB -> col;
-
-                if(curRowA == curRowB && curColA == curColB){
-                    sum = currentNodeA -> value + currentNodeB -> value;
-                    if(sum != 0){
-                        if(matC -> nz == 0){
-                            sp_tuples_node * headNode = malloc(sizeof(sp_tuples_node));
-                            headNode -> row = curRowA;
-                            headNode -> col = curColA;
-                            headNode -> value = sum;
-
-                            matC -> tuples_head = headNode;
-                            headNode -> next = NULL;
-                            prevNode = headNode;
-
-                            matC -> nz += 1;
-                        } else {
-                            sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
-                            newNode -> row = curRowA;
-                            newNode -> col = curColA;
-                            newNode -> value = sum;
-
-                            prevNode -> next = newNode;
-                            newNode -> next = NULL;
-                            prevNode = newNode;
-
-                            matC -> nz += 1;
-                        }
-                    }
-                }
-                currentNodeB = currentNodeB -> next;
-            }
+        //add every value of linked list A to linked list C using set_tuples
+        while(currentNodeA != NULL){
+            set_tuples(matC, currentNodeA -> row, currentNodeA -> col, currentNodeA -> value);
             currentNodeA = currentNodeA -> next;
         }
-    } else {
-        return NULL;
-    }
 
+        //do the same for linked list B. If a value already exists add it to the corresponding coordinates in C
+        int sum;
+        while(currentNodeB != NULL){
+            //if node already doesn't exist, just add the node to linked list
+            if(gv_tuples(matC, currentNodeB -> row, currentNodeB -> col) != 0){
+                set_tuples(matC, currentNodeB -> row, currentNodeB -> col, currentNodeB -> value);
+            //case for if node already exists
+            } else {
+                currentNode = matC -> tuples_head;
+                while(currentNode != NULL){
+                    if(currentNodeB -> row == currentNode -> row && currentNodeB -> col == currentNode -> col){
+                        sum = currentNode -> value + currentNodeB -> value;
+                        set_tuples(matC, currentNodeB -> row, currentNodeB -> col, sum);
+                        break;
+                    }
+                    currentNode = currentNode -> next;
+                }
+            }
+            currentNodeB = currentNodeB -> next;
+        }
+    } 
     if(matC != NULL){
         return matC;
     }
@@ -319,7 +290,7 @@ sp_tuples * add_tuples(sp_tuples * matA, sp_tuples * matB)
 }
 
 
-
+//if i have time tomorrow
 sp_tuples * mult_tuples(sp_tuples * matA, sp_tuples * matB)
 { 
     
@@ -327,29 +298,90 @@ sp_tuples * mult_tuples(sp_tuples * matA, sp_tuples * matB)
 }
 
 
-	
+//free accordingly, each node, then matrix overall
 void destroy_tuples(sp_tuples * mat_t)
 {
-    //set currentNode to the head of the matrix and temp to NULL;
 	sp_tuples_node * currentNode = mat_t -> tuples_head;
     sp_tuples_node * temp = NULL;
     //loop through linked list till NULL pointer is reached
     while(currentNode != NULL){
         //set temp node to next node in linked list
         temp = currentNode -> next;
-        //free current node
         free(currentNode);
         //set current node to temp (the next node in the list)
         currentNode = temp;
     }
-    //set current node to NULL after freeing the last node
     currentNode = NULL;
-    //free the overall tuple object
     free(mat_t);
-    //set tuple to NULL
     mat_t = NULL;
 }  
 
+/*
+ * ----------------------------------------------------------------------------
+ * BELOW ARE FUNCTIONS THAT I USED TO USE BUT THE CODE DIDN'T WORK SO I REWROTE
+ * ----------------------------------------------------------------------------
+ */
+// if(value == 0){
+//         while(currentNode != NULL){
+//             nextNode = currentNode -> next;
+//             if(currentNode -> row == row && currentNode -> col == col){
+//                 if(prevNode == NULL){
+//                     mat_t -> tuples_head = nextNode;
+//                     free(currentNode);
+//                     currentNode = NULL;
+//                     break;
+//                 } else {
+//                     prevNode -> next = nextNode;
+//                     free(currentNode);
+//                     currentNode = NULL;
+//                     break;
+//                 }
+//             } else {
+//                 prevNode = currentNode;
+//                 currentNode = currentNode -> next;
+//             }
+//         }
+//         mat_t -> nz -= 1;
+//     } 
+
+//     currentNode = mat_t -> tuples_head;
+//     while(currentNode != NULL){
+//         if(currentNode -> row == row && currentNode -> col == col){
+//             currentNode -> value = value;
+//             break;
+//         } else {
+//             currentNode = currentNode -> next;
+//         }
+//     } 
+//     currentNode = mat_t -> tuples_head;
+//     prevNode = NULL;
+//     while(currentNode != NULL){
+//         int curNodeIndex = currentNode -> row * mat_t -> m + currentNode -> col;
+//         int swapNodeIndex = row * mat_t -> m + col;
+//         if(prevNode == NULL && curNodeIndex > swapNodeIndex){
+//             sp_tuples_node * newHead = malloc(sizeof(sp_tuples_node));
+//             newHead -> row = row;
+//             newHead -> col = col;
+//             newHead -> value = value;
+//             mat_t -> tuples_head = newHead;
+//             newHead -> next = currentNode; 
+//             mat_t -> nz += 1;
+//             break;
+//         } else {
+//             if(curNodeIndex > swapNodeIndex){
+//                 sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
+//                 newNode -> row = row;
+//                 newNode -> col = col;
+//                 newNode -> value = value;
+//                 newNode -> next = currentNode;
+//                 prevNode -> next = newNode;
+//                 mat_t -> nz += 1;
+//                 break;
+//             }
+//         }
+//         prevNode = currentNode;
+//         currentNode = currentNode -> next;
+//     }
 
 
 
@@ -396,3 +428,44 @@ void destroy_tuples(sp_tuples * mat_t)
 //     }
 //     return myTuple;
 
+// int curRowA, curColA, curRowB, curColB, i, j, sum;
+//         for(i = 0; i < matA -> nz; i++){
+//             currentNodeB = matB -> tuples_head;
+//             for(j = 0; j < matB -> nz; j++){
+//                 curRowA = currentNodeA -> row;
+//                 curColA = currentNodeA -> col;
+//                 curRowB = currentNodeB -> row;
+//                 curColB = currentNodeB -> col;
+
+//                 if(curRowA == curRowB && curColA == curColB){
+//                     sum = currentNodeA -> value + currentNodeB -> value;
+//                     if(sum != 0){
+//                         if(matC -> nz == 0){
+//                             sp_tuples_node * headNode = malloc(sizeof(sp_tuples_node));
+//                             headNode -> row = curRowA;
+//                             headNode -> col = curColA;
+//                             headNode -> value = sum;
+
+//                             matC -> tuples_head = headNode;
+//                             headNode -> next = NULL;
+//                             prevNode = headNode;
+
+//                             matC -> nz += 1;
+//                         } else {
+//                             sp_tuples_node * newNode = malloc(sizeof(sp_tuples_node));
+//                             newNode -> row = curRowA;
+//                             newNode -> col = curColA;
+//                             newNode -> value = sum;
+
+//                             prevNode -> next = newNode;
+//                             newNode -> next = NULL;
+//                             prevNode = newNode;
+
+//                             matC -> nz += 1;
+//                         }
+//                     }
+//                 }
+//                 currentNodeB = currentNodeB -> next;
+//             }
+//             currentNodeA = currentNodeA -> next;
+//         }
